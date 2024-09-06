bd99a1b5506c9e3de8e48917cd031d3791dc2c58
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2024-09-05 20:03:33 -0400

    Update sl-ext.cpp

--- sl-ext.cpp
+++ sl-ext.cpp
@@ -1,4 +1,13 @@
+#include <chrono>
 #include <iostream>
+#include <thread>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
 
 void initialize(){
     cout << "Initializing extension library..." << endl;

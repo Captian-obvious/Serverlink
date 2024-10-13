a5c298e11ccf69163fc1da7db13fa238fc8d6f3b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2024-10-13 16:16:19 -0400

    update compile.sh

--- compile.sh
+++ compile.sh
@@ -10,7 +10,7 @@ OUTFILE="./sl.2"
 OUTFILE2="./libsl-ext.so"
 
 # Compile the shared library
-$COMPILER $FLAGS -shared -o $OUTFILE2 ./sl-ext/sl-ext.cpp
+$COMPILER $FLAGS -shared -o $OUTFILE2 ./sl-ext.cpp
 
 # Compile the main executable and link with the shared library
 $COMPILER $FLAGS ./main.cpp -L. -lsl-ext -Wl,-rpath='./' -o $OUTFILE

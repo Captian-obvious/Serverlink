6c4bbe6e5e1239b4a28c39cc11633405550ffb06
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2024-10-13 15:05:00 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,21 @@
 #!/usr/bin/env bash
+
+# Compile the project
+echo "Compiling..."
+COMPILER=g++
+
+# Main executable
+FLAGS="-g -Werror -fPIC"
+OUTFILE="./sl.2"
+OUTFILE2="./libsl-ext.so"
+
+# Compile the shared library
+$COMPILER $FLAGS -shared -o $OUTFILE2 ./sl-ext/sl-ext.cpp
+
+# Compile the main executable and link with the shared library
+$COMPILER $FLAGS ./main.cpp -L. -lsl-ext -Wl,-rpath='./' -o $OUTFILE
+
+echo "Compiled! Granting permissions..."
+chmod +x $OUTFILE
+chmod +x $OUTFILE2
+echo "Done!"
